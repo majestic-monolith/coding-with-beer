7cb63f55f7ee11fc85505f2f34e42820b2e45065
Valtemir França <valtemir.franca@gran.com>
2025-03-29 05:51:56 -0300

    feat: Configure migrations structure
    Co-authored-by: Daniel Soares <git@danielsoares.dev>
    Co-authored-by: Gustavo Guarda <gustavoguarda@gmail.com>
    Co-authored-by: Pedro Oliveira <pedrophilipe.ti@gmail.com>
    Co-authored-by: Victor Silva <victor.vbs@gmail.com>

---
+++ backend/internal/database/migrations/20250329084115_create_users_table.sql
@@ -0,0 +1,10 @@
+-- migrate:up
+CREATE TABLE users (
+                       id SERIAL PRIMARY KEY,
+                       name VARCHAR(255) NOT NULL,
+                       email VARCHAR(255) NOT NULL,
+                       password VARCHAR(255) NOT NULL,
+);
+
+-- migrate:down
+DROP TABLE users;
